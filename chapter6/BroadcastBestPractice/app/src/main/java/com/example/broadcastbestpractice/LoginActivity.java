1c917014c7081d54abe45474b68d2ab071b445a4
Stevens <longyc2011@gmail.com>
2021-11-22 00:18:32 +0800

    升级一下gradle 和 迁移到androidX。

--- chapter6/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
+++ chapter6/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
@@ -2,9 +2,8 @@ package com.example.broadcastbestpractice;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
