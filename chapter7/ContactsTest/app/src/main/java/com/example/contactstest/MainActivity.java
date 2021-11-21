22963faca793335c82cb640b462ec19e6efd8488
Stevens <longyc2011@gmail.com>
2021-11-21 23:17:54 +0800

    升级一下gradle 和 迁移到androidX。

--- chapter7/ContactsTest/app/src/main/java/com/example/contactstest/MainActivity.java
+++ chapter7/ContactsTest/app/src/main/java/com/example/contactstest/MainActivity.java
@@ -3,23 +3,21 @@ package com.example.contactstest;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.provider.ContactsContract;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     ArrayAdapter<String> adapter;
-
     List<String> contactsList = new ArrayList<>();
 
     @Override
@@ -27,10 +25,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView contactsView = (ListView) findViewById(R.id.contacts_view);
-        adapter = new ArrayAdapter<String>(this, android.R.layout. simple_list_item_1, contactsList);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contactsList);
         contactsView.setAdapter(adapter);
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.READ_CONTACTS }, 1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 1);
         } else {
             readContacts();
         }
@@ -73,5 +71,4 @@ public class MainActivity extends AppCompatActivity {
             default:
         }
     }
-
 }
