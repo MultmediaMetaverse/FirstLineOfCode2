a53dbc9e88c0d532a11b5ea89caa77d36e03d2b7
Stevens <longyc2011@gmail.com>
2021-11-21 23:15:08 +0800

    升级一下gradle 和 迁移到androidX。

--- chapter7/RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
+++ chapter7/RuntimePermissionTest/app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
@@ -4,14 +4,14 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,12 +23,10 @@ public class MainActivity extends AppCompatActivity {
         makeCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    Intent intent = new Intent(Intent.ACTION_CALL);
-                    intent.setData(Uri.parse("tel:10086"));
-                    startActivity(intent);
-                } catch (SecurityException e) {
-                    e.printStackTrace();
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+                } else {
+                    call();
                 }
             }
         });
